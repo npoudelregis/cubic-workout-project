2fa6cd70c6f297d9e00d436ef98a5cfd29e7f092
Naren Poudel <narayanpoudel07@gmail.com>
2019-11-07 12:15:21 -0600

    initial commit

---
+++ src/main/java/com/cubic/workout/cubicworkoutproject/CubicWorkoutProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cubic.workout.cubicworkoutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubicWorkoutProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubicWorkoutProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubic/workout/cubicworkoutproject/CubicWorkoutProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubic.workout.cubicworkoutproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubicWorkoutProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
